45923e00f5a36a89e5524e4dc753f127e94ab72f
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-02-28 22:28:35 +0530

    ᴀsᴜx

---
+++ AsuX/admins.py
@@ -0,0 +1,34 @@
+from typing import List
+from pyrogram.types import Chat
+from Process.Cache.admins import get as gett, set
+from typing import Dict, List
+
+admins: Dict[int, List[int]] = {}
+
+
+def set(chat_id: int, admins_: List[int]):
+    admins[chat_id] = admins_
+
+
+def get(chat_id: int) -> List[int]:
+    if chat_id in admins:
+        return admins[chat_id]
+    return []
+
+
+
+async def get_administrators(chat: Chat) -> List[int]:
+    get = gett(chat.id)
+
+    if get:
+        return get
+    else:
+        administrators = await chat.get_members(filter="administrators")
+        to_set = []
+
+        for administrator in administrators:
+            if administrator.can_manage_voice_chats:
+                to_set.append(administrator.user.id)
+
+        set(chat.id, to_set)
+        return await get_administrators(chat)
