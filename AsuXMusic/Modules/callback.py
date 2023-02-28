1aa22f7ce7ff14cbbab4590b87140f0e86a627df
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-02-28 23:16:41 +0530

    Create callback.py

---
+++ AsuXMusic/Modules/callback.py
@@ -0,0 +1,319 @@
+import yt_dlp
+from Process.queues import QUEUE
+from pyrogram import Client, filters
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+from AsuXMusic import ASSISTANT_USERNAME, BOT_NAME, BOT_USERNAME, OWNER_NAME
+from AsuXMusic.config import (
+    GROUP_SUPPORT,
+    UPDATES_CHANNEL,
+)
+from AsuX.inline import menu_markup, song_download_markup, stream_markup, audio_markup
+
+
+
+@Client.on_callback_query(filters.regex("cbstart"))
+async def cbstart(_, query: CallbackQuery):
+    await query.edit_message_text(
+        f"""ʜᴇʟʟᴏ , ᴍʏ ɴᴀᴍᴇ ɪs {BOT_NAME}.
+
+๏ ᴀ ғᴀsᴛ ᴀɴᴅ ᴩᴏᴡᴇʀғᴜʟ ᴍᴜsɪᴄ ᴩʟᴀʏᴇʀ ʙᴏᴛ ᴡɪᴛʜ sᴏᴍᴇ ᴀᴡᴇsᴏᴍᴇ ғᴇᴀᴛᴜʀᴇs.
+๏ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʜᴇʟᴩ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ ᴛᴏ ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴍʏ ᴄᴏᴍᴍᴀɴᴅs..
+        """,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [                   
+                    InlineKeyboardButton(
+                        "ᴄᴏᴍᴍᴀɴᴅs & ʜᴇʟᴘ ❔", callback_data="cbbasic"
+                    ),
+                ],
+                [
+                    InlineKeyboardButton(
+                        "ʜᴏᴡ ᴛᴏ ᴜsᴇ ᴍᴇ ❓", callback_data="cbhowtouse"
+                    ),
+                  ],[
+                    InlineKeyboardButton(
+                       "ᴜᴘᴅᴀᴛᴇs", url=f"https://t.me/{UPDATES_CHANNEL}"
+                    ),
+                    InlineKeyboardButton(
+                       "sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/{GROUP_SUPPORT}"
+                    )
+                ],[
+                    InlineKeyboardButton(
+                        "➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕",
+                        url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+                    )
+                ]
+            ]
+        ),
+     disable_web_page_preview=True
+    )
+
+
+@Client.on_callback_query(filters.regex("cbhowtouse"))
+async def cbguides(_, query: CallbackQuery):
+    await query.edit_message_text(
+        f"""❓ **ʙᴀsɪᴄ ɢᴜɪᴅᴇ ғᴏʀ ᴜsɪɴɢ ᴛʜɪs ʙᴏᴛ:**
+
+1.) **First, add me to your group.**
+2.) **Then, promote me as administrator and give all permissions except Anonymous Admin.**
+3.) **After promoting me, type /reload in group to refresh the admin data.**
+3.) **Add @{ASSISTANT_USERNAME} to your group or type /userbotjoin to invite her.**
+4.) **Turn on the video chat first before start to play video/music.**
+5.) **Sometimes, reloading the bot by using /reload command can help you to fix some problem.**
+
+📌 **If the userbot not joined to video chat, make sure if the video chat already turned on, or type /userbotleave then type /userbotjoin again.**
+
+💡 **If you have a follow-up questions about this bot, you can tell it on my support chat here: @{GROUP_SUPPORT}***""",
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("🔙 Back", callback_data="cbstart")]]
+        ),
+    )
+
+@Client.on_callback_query(filters.regex("cbbasic"))
+async def cbbasic(_, query: CallbackQuery):
+    await query.edit_message_text(
+        f"""ℹ️ ᴄᴏᴍᴍᴀɴᴅ ᴍᴇɴᴜ
+
+🤷 » /id - ᴛᴏ ɢᴇᴛ ᴜsᴇʀ ɪᴅ
+
+👩🏻‍💼 » /play - ᴛʏᴘᴇ ᴛʜɪs ᴡɪᴛʜ ɢɪᴠᴇ ᴛʜᴇ sᴏɴɢ ᴛɪᴛʟᴇ ᴏʀ ʏᴏᴜᴛᴜʙᴇ ʟɪɴᴋ ᴏʀ ᴀᴜᴅɪᴏ ғɪʟᴇ ᴛᴏ ᴘʟᴀʏ ᴍᴜsɪᴄ. (Remember to don't play YouTube live stream by using this command!, because it will cause unforeseen problems.)
+
+🤷 » /skip - ᴛᴏ sᴋɪᴘ ᴄᴜʀʀᴇɴᴛ sᴏɴɢ
+
+🤷 » /repo - ᴛᴏ ɢᴇᴛ ᴛʜᴇ ʀᴇᴘᴏ
+
+🙋 » /end - ᴛᴏ end ᴘʟᴀʏ sᴏɴɢ ɪɴ ᴠᴄ.""",
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("ɢᴏ ʙᴀᴄᴋ 🏡", callback_data="cbstart")]]
+        ),
+    )
+
+@Client.on_callback_query(filters.regex("cbmenu"))
+async def cbmenu(_, query: CallbackQuery):
+    a = await _.get_chat_member(query.message.chat.id, query.from_user.id)
+    if not a.can_manage_voice_chats:
+        return await query.answer("💡 ᴏɴʟʏ ᴀᴅᴍɪɴ ᴡɪᴛʜ ᴍᴀɴᴀɢᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs ᴘᴇʀᴍɪssɪᴏɴ ᴛʜᴀᴛ ᴄᴀɴ ᴛᴀᴘ ᴛʜɪs ʙᴜᴛᴛᴏɴ !", show_alert=True)
+    chat_id = query.message.chat.id
+    user_id = query.message.from_user.id
+    buttons = menu_markup(user_id)
+    if chat_id in QUEUE:
+          await query.answer("ᴄᴏɴᴛʀᴏʟ ᴘᴀɴᴇʟ ᴏᴘᴇɴᴇᴅ")
+          await query.edit_message_reply_markup(         
+              reply_markup=InlineKeyboardMarkup(buttons),
+          )
+    else:
+        await query.answer("❌ ɴᴏᴛʜɪɴɢ ɪs ᴄᴜʀʀᴇɴᴛʟʏ sᴛʀᴇᴀᴍɪɴɢ", show_alert=True)
+
+@Client.on_callback_query(filters.regex("cbdown"))
+async def cbdown(_, CallbackQuery):
+    await CallbackQuery.answer()
+    callback_data = CallbackQuery.data.strip()
+    callback_request = callback_data.split(None, 1)[1]
+    userid = CallbackQuery.from_user.id
+    videoid, user_id = callback_request.split("|")
+    buttons = song_download_markup(videoid)
+    await CallbackQuery.edit_message_reply_markup(
+        reply_markup=InlineKeyboardMarkup(buttons)
+    )
+
+@Client.on_callback_query(filters.regex(pattern=r"song_back"))
+async def songs_back_helper(client, CallbackQuery):
+    callback_data = CallbackQuery.data.strip()
+    callback_request = callback_data.split(None, 1)[1]
+    userid = CallbackQuery.from_user.id
+    videoid, user_id = callback_request.split("|")
+    buttons = song_download_markup(videoid)
+    return await CallbackQuery.edit_message_reply_markup(
+        reply_markup=InlineKeyboardMarkup(buttons)
+    )
+
+@Client.on_callback_query(filters.regex(pattern=r"gets"))
+async def song_helper_cb(client, CallbackQuery):
+    callback_data = CallbackQuery.data.strip()
+    callback_request = callback_data.split(None, 1)[1]
+    userid = CallbackQuery.from_user.id
+    videoid, user_id = callback_request.split("|")
+    try:
+        await CallbackQuery.answer("soon", show_alert=True)
+    except:
+        pass
+    if stype == "audio":
+        try:
+            formats_available, link = await YouTube.formats(
+                videoid, True
+            )
+        except:
+            return await CallbackQuery.edit_message_text("ᴅᴏᴡɴʟᴏᴀᴅ ᴀᴜᴅɪᴏ")
+        keyboard = InlineKeyboard()
+        done = []
+        for x in formats_available:
+            check = x["format"]
+            if "audio" in check:
+                if x["filesize"] is None:
+                    continue
+                form = x["format_note"].title()
+                if form not in done:
+                    done.append(form)
+                else:
+                    continue
+                sz = convert_bytes(x["filesize"])
+                fom = x["format_id"]
+                keyboard.row(
+                    InlineKeyboardButton(
+                        text=f"{form} ǫᴜᴀʟɪᴛʏ ᴀᴜᴅɪᴏ = {sz}",
+                        callback_data=f"song_download {stype}|{fom}|{videoid}",
+                    ),
+                )
+        keyboard.row(
+            InlineKeyboardButton(
+                text="🔙 ʙᴀᴄᴋ",
+                callback_data=f"song_back {stype}|{videoid}",
+            ),
+            InlineKeyboardButton(
+                text="✖️ ᴄʟᴏsᴇ ", callback_data=f"cls"
+            ),
+        )
+        return await CallbackQuery.edit_message_reply_markup(
+            reply_markup=keyboard
+        )
+    else:
+        try:
+            formats_available, link = await YouTube.formats(
+                videoid, True
+            )
+        except Exception as e:
+            print(e)
+            return await CallbackQuery.edit_message_text("ᴅᴏᴡɴʟᴏᴀᴅ ᴠɪᴅᴇᴏ")
+        keyboard = InlineKeyboard()
+        done = [160, 133, 134, 135, 136, 137, 298, 299, 264, 304, 266]
+        for x in formats_available:
+            check = x["format"]
+            if x["filesize"] is None:
+                continue
+            if int(x["format_id"]) not in done:
+                continue
+            sz = convert_bytes(x["filesize"])
+            ap = check.split("-")[1]
+            to = f"{ap} = {sz}"
+            keyboard.row(
+                InlineKeyboardButton(
+                    text=to,
+                    callback_data=f"song_download {stype}|{x['format_id']}|{videoid}",
+                )
+            )
+        keyboard.row(
+            InlineKeyboardButton(
+                text="🔙 ʙᴀᴄᴋ",
+                callback_data=f"song_back {stype}|{videoid}",
+            ),
+            InlineKeyboardButton(
+                text="✖️ ᴄʟᴏsᴇ", callback_data=f"cls"
+            ),
+        )
+        return await CallbackQuery.edit_message_reply_markup(
+            reply_markup=keyboard
+        )
+
+@Client.on_callback_query(filters.regex(pattern=r"song_download"))
+async def song_download_cb(client, CallbackQuery):
+    try:
+        await CallbackQuery.answer("ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ")
+    except:
+        pass
+    callback_data = CallbackQuery.data.strip()
+    callback_request = callback_data.split(None, 1)[1]
+    stype, format_id, videoid = callback_request.split("|")
+    mystic = await CallbackQuery.edit_message_text(_["song_8"])
+    yturl = f"https://www.youtube.com/watch?v={vidid}"
+    with yt_dlp.YoutubeDL({"quiet": True}) as ytdl:
+        x = ytdl.extract_info(yturl, download=False)
+    title = (x["title"]).title()
+    title = re.sub("\W+", " ", title)
+    thumb_image_path = await CallbackQuery.message.download()
+    duration = x["duration"]
+    if stype == "video":
+        thumb_image_path = await CallbackQuery.message.download()
+        width = CallbackQuery.message.photo.width
+        height = CallbackQuery.message.photo.height
+        try:
+            file_path = await YouTube.download(
+                yturl,
+                mystic,
+                songvideo=True,
+                format_id=format_id,
+                title=title,
+            )
+        except Exception as e:
+            return await mystic.edit_text("error".format(e))
+        med = InputMediaVideo(
+            media=file_path,
+            duration=duration,
+            width=width,
+            height=height,
+            thumb=thumb_image_path,
+            caption=title,
+            supports_streaming=True,
+        )
+        await mystic.edit_text("video")
+        await app.send_chat_action(
+            chat_id=CallbackQuery.message.chat.id,
+            action="upload_video",
+        )
+        try:
+            await CallbackQuery.edit_message_media(media=med)
+        except Exception as e:
+            print(e)
+            return await mystic.edit_text("sᴏᴏɴᴠɪᴅᴇᴏ")
+        os.remove(file_path)
+    elif stype == "audio":
+        try:
+            filename = await YouTube.download(
+                yturl,
+                mystic,
+                songaudio=True,
+                format_id=format_id,
+                title=title,
+            )
+        except Exception as e:
+            return await mystic.edit_text("error".format(e))
+        med = InputMediaAudio(
+            media=filename,
+            caption=title,
+            thumb=thumb_image_path,
+            title=title,
+            performer=x["uploader"],
+        )
+        await mystic.edit_text("audio")
+        await app.send_chat_action(
+            chat_id=CallbackQuery.message.chat.id,
+            action="upload_audio",
+        )
+        try:
+            await CallbackQuery.edit_message_media(media=med)
+        except Exception as e:
+            print(e)
+            return await mystic.edit_text("sᴏᴏɴᴀᴜᴅɪᴏ")
+        os.remove(filename)
+
+@Client.on_callback_query(filters.regex("cbhome"))
+async def cbhome(_, query: CallbackQuery):
+    a = await _.get_chat_member(query.message.chat.id, query.from_user.id)
+    if not a.can_manage_voice_chats:
+        return await query.answer("💡 ᴏɴʟʏ ᴀᴅᴍɪɴ ᴡɪᴛʜ ᴍᴀɴᴀɢᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs ᴘᴇʀᴍɪssɪᴏɴ ᴛʜᴀᴛ ᴄᴀɴ ᴛᴀᴘ ᴛʜɪs ʙᴜᴛᴛᴏɴ !", show_alert=True)
+    chat_id = query.message.chat.id
+    user_id = query.message.from_user.id
+    buttons = stream_markup(user_id, dlurl)
+    if chat_id in QUEUE:
+          await query.answer("Back")
+          await query.edit_message_reply_markup(         
+              reply_markup=InlineKeyboardMarkup(buttons),
+          )
+    else:
+        await query.answer("❌ ɴᴏᴛʜɪɴɢ ɪs ᴄᴜʀʀᴇɴᴛʟʏ sᴛʀᴇᴀᴍɪɴɢ", show_alert=True)
+    
+@Client.on_callback_query(filters.regex("cls"))
+async def close(_, query: CallbackQuery):
+    a = await _.get_chat_member(query.message.chat.id, query.from_user.id)
+    if not a.can_manage_voice_chats:
+        return await query.answer("💡 ᴏɴʟʏ ᴀᴅᴍɪɴ ᴡɪᴛʜ ᴍᴀɴᴀɢᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs ᴘᴇʀᴍɪssɪᴏɴ ᴛʜᴀᴛ ᴄᴀɴ ᴛᴀᴘ ᴛʜɪs ʙᴜᴛᴛᴏɴ !", show_alert=True)
+    await query.message.delete()
