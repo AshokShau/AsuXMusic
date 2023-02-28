fdf0af780dde153e88934cda782dc17a54e1b4c7
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-03-01 02:47:43 +0530

    Update __init__.py

--- AsuXMusic/__init__.py
+++ AsuXMusic/__init__.py
@@ -5,7 +5,7 @@ from AsuXMusic.config import API_HASH, API_ID, BOT_TOKEN, SESSION_NAME
 from AsuXMusic.Modules import ALL_MODULES
 
 bot = Client(
-    "AsuXMusic", API_ID, API_HASH, bot_token=BOT_TOKEN, plugins=dict(str(ALL_MODULES))
+    "AsuXMusic", API_ID, API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="AsuXMusic.*"),
 )
 
 Abishnoi = Client(
