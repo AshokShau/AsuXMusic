a1626b1f52cc9ceec9ab3d483a50d4b238396012
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-02-28 23:44:42 +0530

    Create config.py

---
+++ AsuXMusic/config.py
@@ -0,0 +1,28 @@
+import os
+from os import getenv
+from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+load_dotenv()
+
+admins = {}
+
+SESSION_NAME = getenv("SESSION_NAME", "")
+
+BOT_TOKEN = ""
+API_ID = int(getenv("API_ID", "8186557"))
+API_HASH = getenv("API_HASH", "efd77b34c69c164ce158037ff5a0d117")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "AbishnoiMF")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "Abishnoi_bots")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", " ").split()))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "120"))
+
+IMG_1 = getenv("IMG_1", "https://te.legra.ph/file/31e9ecee16a46575267a4.jpg")
+IMG_2 = getenv("IMG_2", "https://te.legra.ph/file/d744707634106cf76627a.jpg")
+IMG_3 = getenv("IMG_3", "https://telegra.ph/file/f02efde766160d3ff52d6.png")
+IMG_4 = getenv("IMG_4", "https://telegra.ph/file/be5f551acb116292d15ec.png")
+IMG_5 = getenv("IMG_5", "https://telegra.ph/file/c3401a572375b569138c3.png")
+IMG_6 = getenv("IMG_6", "https://telegra.ph/file/d8f8fc1de9110b93ca94c.jpg")
+YOUTUBE_IMG_URL = getenv("YOUTUBE_IMG_URL", "https://te.legra.ph/file/314097c1825e2cddfc6cd.jpg")
