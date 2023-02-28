18769fe93d9a62cf92a87644e457307197fd8697
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-02-28 23:20:45 +0530

    Update callback.py

--- AsuXMusic/Modules/callback.py
+++ AsuXMusic/Modules/callback.py
@@ -10,6 +10,82 @@ from AsuXMusic.config import (
 from AsuX.inline import menu_markup, song_download_markup, stream_markup, audio_markup
 
 
+from pyrogram import Client, errors
+from pyrogram.types import (
+    InlineQuery,
+    InlineQueryResultArticle,
+    InputTextMessageContent,
+)
+from youtubesearchpython import VideosSearch
+
+
+def ytsearch(query):
+    try:
+        search = VideosSearch(query, limit=1).result()
+        data = search["result"][0]
+        songname = data["title"]
+        url = data["link"]
+        duration = data["duration"]
+        thumbnail = f"https://i.ytimg.com/vi/{data['id']}/hqdefault.jpg"
+        videoid = data["id"]
+        return [songname, url, duration, thumbnail, videoid]
+    except Exception as e:
+        print(e)
+        return 0
+
+@Client.on_inline_query()
+async def inline(client: Client, query: InlineQuery):
+    answerss = (
+      [
+        InlineQueryResultArticle(
+            title="Pause Stream",
+            description=f"ᴘᴀᴜsᴇ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴘʟᴀʏᴏᴜᴛ ᴏɴ ɢʀᴏᴜᴘ ᴄᴀʟʟ.",
+            thumb_url="https://telegra.ph/file/c0a1c789def7b93f13745.png",
+            input_message_content=InputTextMessageContent("/pause"),
+        ),
+        InlineQueryResultArticle(
+            title="Resume Stream",
+            description=f"ʀᴇsᴜᴍᴇ ᴛʜᴇ ᴏɴɢᴏɪɴɢ ᴘʟᴀʏᴏᴜᴛ ᴏɴ ɢʀᴏᴜᴘ ᴄᴀʟʟ.",
+            thumb_url="https://telegra.ph/file/02d1b7f967ca11404455a.png",
+            input_message_content=InputTextMessageContent("/resume"),
+        ),
+        InlineQueryResultArticle(
+            title="Mute Stream",
+            description=f"ᴍᴜᴛᴇ ᴛʜᴇ ᴏɴɢᴏɪɴɢ ᴘʟᴀʏᴏᴜᴛ ᴏɴ ɢʀᴏᴜᴘ ᴄᴀʟʟ.",
+            thumb_url="https://telegra.ph/file/66516f2976cb6d87e20f9.png",
+            input_message_content=InputTextMessageContent("/mute"),
+        ),
+        InlineQueryResultArticle(
+            title="Unmute Stream",
+            description=f"ᴜɴᴍᴜᴛᴇ ᴛʜᴇ ᴏɴɢᴏɪɴɢ ᴘʟᴀʏᴏᴜᴛ ᴏɴ ɢʀᴏᴜᴘ ᴄᴀʟʟ.",
+            thumb_url="https://telegra.ph/file/3078794f9341ffd582e18.png",
+            input_message_content=InputTextMessageContent("/unmute"),
+        ),
+        InlineQueryResultArticle(
+            title="Skip Stream",
+            description=f"sᴋɪᴘ ᴛᴏ ɴᴇxᴛ ᴛʀᴀᴄᴋ. | ғᴏʀ sᴘᴇᴄɪғɪᴄ ᴛʀᴀᴄᴋ ɴᴜᴍʙᴇʀ: /skip [ɴᴜᴍʙᴇʀ] ",
+            thumb_url="https://telegra.ph/file/98b88e52bc625903c7a2f.png",
+            input_message_content=InputTextMessageContent("/skip"),
+        ),
+        InlineQueryResultArticle(
+            title="End Stream",
+            description="sᴛᴏᴘ ᴛʜᴇ ᴏɴɢᴏɪɴɢ ᴘʟᴀʏᴏᴜᴛ ᴏɴ ɢʀᴏᴜᴘ ᴄᴀʟʟ.",
+            thumb_url="https://telegra.ph/file/d2eb03211baaba8838cc4.png",
+            input_message_content=InputTextMessageContent("/stop"),
+        ),
+      ]
+    )
+    search_query = query.query.lower().strip().rstrip()
+
+    if search_query == "":
+        await client.answer_inline_query(
+            query.id,
+            results=answerss,
+            switch_pm_text="ᴛʏᴘᴇ ᴛʜᴇ ɴᴀᴍᴇ ᴏғ ᴛʜᴇ sᴏɴɢ/Video ʏᴏᴜᴛᴜʙᴇ...",
+            switch_pm_parameter="help",
+            cache_time=0,
+        )
+
 
 @Client.on_callback_query(filters.regex("cbstart"))
 async def cbstart(_, query: CallbackQuery):
