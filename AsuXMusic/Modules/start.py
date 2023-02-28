6c14dde2ab13b7a6a97faebbe147043d6f84f70c
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-03-01 01:45:41 +0530

    Create start.py

---
+++ AsuXMusic/Modules/start.py
@@ -0,0 +1,87 @@
+from pyrogram import filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from AsuXMusic inport BOT_NAME, BOT_USERNAME, OWNER_NAME, ASSISTANT_USERNAME, bot as Abishnoi
+from AsuXMusic.config import (
+    GROUP_SUPPORT,
+    UPDATES_CHANNEL,
+)
+from AsuX.filters import other_filters2
+from time import time
+from Process.filters import command
+from datetime import datetime
+from AsuX.decorators import authorized_users_only
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ("week", 60 * 60 * 24 * 7),
+    ("day", 60 ** 2 * 24),
+    ("hour", 60 ** 2),
+    ("min", 60),
+    ("sec", 1),
+)
+
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return "inf"
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append("{} {}{}".format(amount, unit, "" if amount == 1 else "s"))
+    return ", ".join(parts)
+
+
+@Abishnoi.on_message(command(["start", "help"]) & ~filters.group)
+async def start(_, message: Message):
+        await message.reply_text(
+        f"""ʜᴇʟʏ {message.from_user.mention()}, 
+                  ᴍʏ ɴᴀᴍᴇ ɪs {BOT_NAME}.
+๏ ᴀ ғᴀsᴛ ᴀɴᴅ ᴩᴏᴡᴇʀғᴜʟ ᴍᴜsɪᴄ ᴩʟᴀʏᴇʀ ʙᴏᴛ ᴡɪᴛʜ sᴏᴍᴇ ᴀᴡᴇsᴏᴍᴇ ғᴇᴀᴛᴜʀᴇs.
+๏ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʜᴇʟᴩ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ ᴛᴏ ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴍʏ ᴄᴏᴍᴍᴀɴᴅs..
+        """,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [                   
+                    InlineKeyboardButton(
+                        "ᴄᴏᴍᴍᴀɴᴅs ❔", callback_data="cbbasic"
+                    ),
+                ],
+                [
+                    InlineKeyboardButton(
+                        "ʜᴏᴡ ᴛᴏ ᴜsᴇ ᴍᴇ ❓", callback_data="cbhowtouse"
+                    ),
+                  ],[
+                    InlineKeyboardButton(
+                       "ᴜᴘᴅᴀᴛᴇs", url=f"https://t.me/{UPDATES_CHANNEL}"
+                    ),
+                    InlineKeyboardButton(
+                       "sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/{GROUP_SUPPORT}"
+                    )
+                ],[
+                    InlineKeyboardButton(
+                        "➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴄʜᴀᴛ ➕",
+                        url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+                    )
+                ]
+            ]
+        ),
+     disable_web_page_preview=True
+    )
+
+
+@Abishnoi.on_message(command(["repo", "source"]))
+async def help(client: Abishnoi, message: Message):
+    await message.reply_photo(
+        photo=f"https://te.legra.ph/file/56557bd94afbe895ae483.jpg",
+        caption=f"""ʜᴇʀᴇ ɪs ᴛʜᴇ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ғᴏʀᴋ ᴀɴᴅ ɢɪᴠᴇ sᴛᴀʀs ✨""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        " ʀᴇᴘᴏ ⚒️", url=f"https://github.com/Abishnoi69/AsuXMusic")
+                ]
+            ]
+        ),
+    )
