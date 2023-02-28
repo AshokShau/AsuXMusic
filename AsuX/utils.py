9edbba161e9ac560f92423305d72eada61fe3225
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-03-01 00:36:33 +0530

    Update utils.py

--- AsuX/utils.py
+++ AsuX/utils.py
@@ -1,6 +1,6 @@
 import os
 import asyncio
-from Process.main import bot, call_py
+from AsuXMusic import bot, call_py
 from pytgcalls.types import Update
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
 from Process.queues import QUEUE, clear_queue, get_queue, pop_an_item
