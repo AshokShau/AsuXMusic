e917e36026f46625bb08fd3bedf54eac28f72d14
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-03-01 00:18:02 +0530

    Create __init__.py

---
+++ AsuXMusic/__init__.py
@@ -0,0 +1,29 @@
+from pyrogram import Client
+from AsuXMusic.config import API_ID, API_HASH, BOT_TOKEN, SESSION_NAME
+from pytgcalls import PyTgCalls, idle
+
+bot = Client(
+    "AsuXMusic",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="AsuXMusic.Modules"),
+    )
+
+Abishnoi = Client(
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_name=SESSION_NAME,
+    
+    )
+
+user = PyTgCalls(Abishnoi,
+    cache_duration=100,
+    overload_quiet_mode=True,)
+
+call_py = PyTgCalls(Abishnoi, overload_quiet_mode=True)
+
+with Client("AsuXMusic", API_ID, API_HASH, bot_token=BOT_TOKEN) as app:
+    me_bot = app.get_me()
+with Abishnoi as app:
+    me_Abishnoi = app.get_me()
