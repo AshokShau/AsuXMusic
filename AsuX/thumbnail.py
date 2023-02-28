2f43d732797c38770b677770be3944abbce4eb1a
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-03-01 00:27:00 +0530

    Update thumbnail.py

--- AsuX/thumbnail.py
+++ AsuX/thumbnail.py
@@ -7,7 +7,7 @@ import aiohttp
 from PIL import (Image, ImageDraw, ImageEnhance, ImageFilter,
                  ImageFont, ImageOps)
 from youtubesearchpython.__future__ import VideosSearch
-from AsuXMusic inport BOT_NAME
+from AsuXMusic import BOT_NAME
 from AsuXMusic.config import YOUTUBE_IMG_URL
 
 def changeImageSize(maxWidth, maxHeight, image):
