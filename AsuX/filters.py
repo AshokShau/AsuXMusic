3d540343185bff011cc5e30d1ff333513f197852
Abishnoi69 <Abishnoi69@gmail.com>
2023-03-02 08:45:09 +0000

    🥀

--- AsuX/filters.py
+++ AsuX/filters.py
@@ -6,9 +6,7 @@ COMMAND_PREFIXES = ["/", "!", ".", "*", "+"]
 
 
 other_filters = filters.group & ~filters.via_bot & ~filters.forwarded
-other_filters2 = (
-    filters.private & ~filters.via_bot & ~filters.forwarded
-)
+other_filters2 = filters.private & ~filters.via_bot & ~filters.forwarded
 
 
 def command(commands: Union[str, List[str]]):
