de4790dce67c72c2a5d68243c057f0f8f18036b6
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-03-02 14:14:53 +0530

    Update filters.py

--- AsuX/filters.py
+++ AsuX/filters.py
@@ -2,12 +2,12 @@ from typing import List, Union
 
 from pyrogram import filters
 
-COMMAND_PREFIXES = "/"
+COMMAND_PREFIXES = ["/", "!", ".", "*", "+"]
 
 
-other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
+other_filters = filters.group & ~filters.via_bot & ~filters.forwarded
 other_filters2 = (
-    filters.private & ~filters.edited & ~filters.via_bot & ~filters.forwarded
+    filters.private & ~filters.via_bot & ~filters.forwarded
 )
 
 
